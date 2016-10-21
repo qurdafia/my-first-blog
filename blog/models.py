fa40ade9eb697693e1aa6629568c43689e6842a0
qurdafia <fragrancegarden@gmail.com>
2016-10-21 18:00:14 +0800

    Fixing the comment section

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
@@ -30,6 +33,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
--- blog/views.py
+++ blog/views.py
@@ -82,11 +82,11 @@ def add_comment_to_post(request, pk):
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
-    return redirect('blog.views.post_detail', pk=comment.post.pk)
+    return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
     comment.delete()
-    return redirect('blog.views.post_detail', pk=post_pk)
+    return redirect('post_detail', pk=post_pk)
