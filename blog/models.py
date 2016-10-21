54dd862d0f5446a8be475ac8a69ed66960b46cbb
qurdafia <fragrancegarden@gmail.com>
2016-10-21 18:09:13 +0800

    Updating the comment agian

--- blog/models.py
+++ blog/models.py
@@ -12,11 +12,11 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
-
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
 #Comment Model
 
