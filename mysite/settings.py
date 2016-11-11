78d52c13bf50fe98d07dd62befb269fff3e30e12
qurdafia <fragrancegarden@gmail.com>
2016-11-11 17:46:20 +0800

    Changing the model

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 LOGIN_REDIRECT_URL = '/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/home/mormash/my-first-blog/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/mormash/my-first-blog/media_cdn')
 
 MEDIA_URL = '/media/'
