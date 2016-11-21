b5ee028faa8f4b0b2f1e34933b88e9ddc6812952
qurdafia <fragrancegarden@gmail.com>
2016-11-21 10:13:17 +0800

    Adjusting the title margin

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,6 +81,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'mormash',
+        #'USER': 'mormash',
+        #'PASSWORD': '22MatetMatet22',
+        #'HOST': 'mormash.mysql.pythonanywhere-services.com',  # Or an IP Address that your DB is hosted on
+        #'PORT': '3306',
     }
 }
 
