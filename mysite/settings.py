a3294f139b98e43ac077404ca391d3202686d90f
qurdafia <fragrancegarden@gmail.com>
2016-11-26 06:44:53 +0000

    Changes in the details template.

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'mormash',
-        #'USER': 'mormash',
-        #'PASSWORD': '22MatetMatet22',
-        #'HOST': 'mormash.mysql.pythonanywhere-services.com',  # Or an IP Address that your DB is hosted on
-        #'PORT': '3306',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mormash$chipining',
+        'USER': 'mormash',
+        'PASSWORD': '22MatetMatet22',
+        'HOST': 'mormash.mysql.pythonanywhere-services.com',  # Or an IP Address that your DB is hosted on
+        'PORT': '',
     }
 }
 
